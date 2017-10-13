06111fcbf65bd93047cdb034e27211c1851b244a
joehou <emailjoehou@gmail.com>
2017-10-12 23:06:21 -0700

    feat: 404 for deleted post
    Test to see if object returned from api is an empty object. if empty display 404

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -17,32 +17,35 @@ class PostView extends Component {
   }
 
   render() {
-    return !this.props.post ?(
+    return !this.props.post  ?(
         <div>loading</div>
       )
       :(
          <div className="col-9 selected-post">
-          <PostItem post={this.props.post}
-            onDownVotePost={this.props.downVotePost}
-            onUpVotePost={this.props.upVotePost}
-            onSelectPost={this.props.selectPost}
-            onDeletePost={this.props.deletePost}
-          />
-          <CommentsList comments={this.props.comments}
-            onDownVoteComment={this.props.downVoteComment}
-            onUpVoteComment={this.props.upVoteComment}
-            onDeleteComment={this.props.removeComment}
-            onOpenCreateCommentModal={this.props.openCreateCommentModal}
-            onOpenEditCommentModal={this.props.openEditCommentModal}
-          />
-          <CommentModal
-            commentsModalOpen = {this.props.commentsModalOpen}
-            onUpdateComment = {this.props.updateComment}
-            resetEditComment = {this.props.resetEditComment}
-            onCreateComment = {this.props.createComment}
-            currentComment = {this.props.currentComment}
-            selectedPost={this.props.selectedPost}
-          />
+           {Object.keys(this.props.post).length ===0 && <h1>404 post deleted</h1>}
+           {Object.keys(this.props.post).length !==0 && <div>
+             <PostItem post={this.props.post}
+              onDownVotePost={this.props.downVotePost}
+              onUpVotePost={this.props.upVotePost}
+              onSelectPost={this.props.selectPost}
+              onDeletePost={this.props.removePost}
+            />
+            <CommentsList comments={this.props.comments}
+              onDownVoteComment={this.props.downVoteComment}
+              onUpVoteComment={this.props.upVoteComment}
+              onDeleteComment={this.props.removeComment}
+              onOpenCreateCommentModal={this.props.openCreateCommentModal}
+              onOpenEditCommentModal={this.props.openEditCommentModal}
+            />
+            <CommentModal
+              commentsModalOpen = {this.props.commentsModalOpen}
+              onUpdateComment = {this.props.updateComment}
+              resetEditComment = {this.props.resetEditComment}
+              onCreateComment = {this.props.createComment}
+              currentComment = {this.props.currentComment}
+              selectedPost={this.props.selectedPost}
+            />
+           </div>}
         </div>
       )
   }
